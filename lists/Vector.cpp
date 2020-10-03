bce6bdec9881efecdb9f669e51fabcae30e4aab2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-03 18:39:48 +0100

    Declared copy constructor and assignment of vectoryclass

--- lists/Vector.cpp
+++ lists/Vector.cpp
@@ -9,3 +9,20 @@ Vector<Object>::Vector(std::size_t size)
         : mSize{size}, mCapacity{size + SPARE_CAPACITY} {
     objects = new Object[mCapacity];
 }
+
+template<typename Object>
+Vector<Object>::Vector(const Vector<Object> &rhs)
+        : mSize{rhs.mSize}, mCapacity{rhs.mCapacity}, objects{nullptr} {
+    objects = new Object[mCapacity];
+
+    for (std::size_t i{}; i < mSize; ++i)
+        objects[i] = rhs.objects[i];
+}
+
+template<typename Object>
+Vector<Object> &Vector<Object>::operator=(const Vector<Object> &rhs) {
+    Vector copy = rhs;
+    std::swap(*this, copy);
+
+    return *this;
+}
