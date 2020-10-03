06cf46f29195ae5ccdeb41e42ca69796359b41bb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-03 18:29:19 +0100

    Declared constructor

--- lists/Vector.cpp
+++ lists/Vector.cpp
@@ -3,3 +3,9 @@
 //
 
 #include "Vector.h"
+
+template<typename Object>
+Vector<Object>::Vector(std::size_t size)
+        : mSize{size}, mCapacity{size + SPARE_CAPACITY} {
+    objects = new Object[mCapacity];
+}
