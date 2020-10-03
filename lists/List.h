e61618f081ff0db75befd984b31d0ec27f65b02e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-03 20:07:09 +0100

    Provided the header file of the doubly-linked list class

--- lists/List.h
+++ lists/List.h
@@ -5,9 +5,76 @@
 #ifndef DSAA_LIST_H
 #define DSAA_LIST_H
 
+#include <cstddef>
 
+template<typename Object>
 class List {
+private:
+    struct Node {
+    };
+    std::size_t mSize;
+    Node *head;
+    Node *tail;
+public:
+    class const_iterator {
+    };
 
+    class iterator : public const_iterator {
+    };
+
+    List();
+
+    List(const List &rhs);
+
+    List &operator=(const List &rhs);
+
+    List(List &&rhs);
+
+    List &operator=(List &&rhs);
+
+    ~List();
+
+    iterator begin();
+
+    const_iterator begin() const;
+
+    iterator end();
+
+    const_iterator end() const;
+
+    std::size_t size() const;
+
+    bool empty() const;
+
+    void clear();
+
+    Object &front();
+
+    const Object &front() const;
+
+    Object &back();
+
+    const Object &back() const;
+
+    void push_front(const Object &item);
+
+    void push_front(Object &&item);
+
+    void push_back(const Object &item);
+
+    void push_back(Object &&item);
+
+    void pop_front();
+
+    void pop_back();
+
+    iterator insert(iterator iter, const Object &item);
+
+    iterator insert(iterator iter, Object &&item);
+
+    iterator erase(iterator iter);
+
+    iterator erase(iterator from, iterator to);
 };
 
 
