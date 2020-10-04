6af0435fd1c22f6579bd1d323d6ed2a140ed4c1a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-04 10:08:12 +0100

    Added a leetcode challenge solution

---
+++ leetcode/SquaresOfSortedArray.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Oyekunle Oloyede on 04/10/2020.
+//
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    static std::vector<int> sortedSquares(std::vector<int> &A) {
+        std::vector<int> squares(0);
+
+        for (auto item: A)
+            squares.push_back(item * item);
+
+        std::sort(squares.begin(), squares.end());
+
+        return squares;
+    }
+};
