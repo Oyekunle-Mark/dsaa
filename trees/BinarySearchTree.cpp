e6e59b0177e78c8503d8a65e58cf565535f47f1a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-04 19:40:03 +0100

    Some implementation of public methods

--- trees/BinarySearchTree.cpp
+++ trees/BinarySearchTree.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "BinarySearchTree.h"
+
+template<typename Comparable>
+bool BinarySearchTree<Comparable>::contains(const Comparable &item) const {
+    return contains(item, root);
+}
+
+template<typename Comparable>
+void BinarySearchTree<Comparable>::insert(const Comparable &item) {
+    return insert(item, root);
+}
+
+template<typename Comparable>
+void BinarySearchTree<Comparable>::remove(const Comparable &item) {
+    return remote(item, root);
+}
