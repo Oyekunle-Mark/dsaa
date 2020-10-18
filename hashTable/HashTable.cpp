305a583fa60e8f3ae3d401303e69d19ceee05df0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-18 13:41:53 +0100

    Added makeEmpty and contains method

--- hashTable/HashTable.cpp
+++ hashTable/HashTable.cpp
@@ -14,3 +14,15 @@ std::size_t HashTable<HashedObj>::myHash(const HashedObj &item) const {
     static hash<HashedObj> hashFunction{};
     return hashFunction(item) % theLists.size();
 }
+
+template<typename HashedObj>
+void HashTable<HashedObj>::makeEmpty() {
+    for (auto &thisList: theLists)
+        thisList.clear();
+}
+
+template<typename HashedObj>
+bool HashTable<HashedObj>::contains(const HashedObj &item) const {
+    auto &whichList = theLists[myHash(item)];
+    return std::find(std::begin(whichList), std::end(whichList), item) != std::end(whichList);
+}
