686bbee6e7be70d9505a3045d5ef30503e235a25
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-18 13:18:33 +0100

    Added employee class

--- hashTable/Employee.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Oyekunle Oloyede on 18/10/2020.
-//
-
-#include "Employee.h"
--- hashTable/Employee.h
+++ hashTable/Employee.h
@@ -5,9 +5,30 @@
 #ifndef DSAA_EMPLOYEE_H
 #define DSAA_EMPLOYEE_H
 
+#include <string>
+#include <utility>
 
 class Employee {
+public:
+    explicit Employee(std::string name, double salary, std::size_t seniority)
+            : name{std::move(name)}, salary{salary}, seniority{seniority} {}
 
+    const std::string &getName() const {
+        return name;
+    }
+
+    bool operator==(const Employee &rhs) const {
+        return getName() == rhs.getName();
+    }
+
+    bool operator!=(const Employee &rhs) const {
+        return !(*this == rhs);
+    }
+
+private:
+    std::string name;
+    double salary;
+    std::size_t seniority;
 };
 
 
