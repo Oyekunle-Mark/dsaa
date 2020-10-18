62984416c739f56a1bb8bf7af54c2409c7910133
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-18 13:29:04 +0100

    Added constructor and hash function

--- hashTable/HashTable.cpp
+++ hashTable/HashTable.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "HashTable.h"
+#include "hash.h"
+
+template<typename HashedObj>
+HashTable<HashedObj>::HashTable(int size)
+        : currentSize{static_cast<std::size_t>(size)} {}
+
+template<typename HashedObj>
+std::size_t HashTable<HashedObj>::myHash(const HashedObj &item) const {
+    static hash<HashedObj> hashFunction{};
+    return hashFunction(item) % theLists.size();
+}
