08f098b61d6b22b4686813c3c99ac31ffa12233d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-18 12:50:40 +0100

    Type declaration for separate chaining hash table

--- hashTable/HashTable.h
+++ hashTable/HashTable.h
@@ -5,9 +5,31 @@
 #ifndef DSAA_HASHTABLE_H
 #define DSAA_HASHTABLE_H
 
+#include <vector>
+#include <list>
 
+template<typename HashedObj>
 class HashTable {
+public:
+    explicit HashTable(int size = 10);
 
+    bool contains(const HashedObj &item) const;
+
+    void makeEmpty();
+
+    bool insert(const HashedObj &item);
+
+    bool insert(HashedObj &&item);
+
+    bool remove(const HashedObj &item);
+
+private:
+    std::vector<std::list<HashedObj>> theLists;
+    std::size_t currentSize;
+
+    void rehash();
+
+    size_t myHash(const HashedObj &item) const;
 };
 
 
