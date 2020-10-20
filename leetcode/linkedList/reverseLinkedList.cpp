dc7e415c83b092a8a3c8c5994a7fd81c01c20bb3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-20 17:04:57 +0100

    Reverse a linked list iterative solution

--- leetcode/linkedList/reverseLinkedList.cpp
+++ leetcode/linkedList/reverseLinkedList.cpp
@@ -11,6 +11,10 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
+ *
+ * Example:
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
  */
 class ReverseLinkedList {
 private:
@@ -27,6 +31,17 @@ private:
 
 public:
     static ListNode *reverseList(ListNode *head) {
+        if (!head) return head; // if the list is empty
 
+        auto tail = head; // set the head to tail to begin with
+
+        while (tail->next) { // while there is a node after tail
+            auto newHead = tail->next; // set newHead to the next node after tail
+            tail->next = tail->next->next; // remove the newHead and point tail to the node after newHead
+            newHead->next = head; // point newHead's next to the old head i.e, add newHead at the head of the linked list
+            head = newHead; // update the head pointer variable to point at newHead
+        }
+
+        return head;
     }
 };
