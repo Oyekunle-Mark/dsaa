bc3be5b845c40ab18aeb3703169da6e2d354dbd3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-20 09:57:38 +0100

    New linked list class

---
+++ leetcode/linkedList/linkedListCycleII.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Oyekunle Oloyede on 20/10/2020.
+//
+
+//
+// Created by Oyekunle Oloyede on 20/10/2020.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class LinkedListCycleII {
+private:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        explicit ListNode(int x) : val(x), next(nullptr) {}
+    };
+
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+    }
+};
