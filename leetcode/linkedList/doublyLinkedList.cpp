53e6018361bb8b333d8e3484f461b04c964684fd
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-22 00:21:51 +0100

    Provided add at head and add at tail methods

--- leetcode/linkedList/doublyLinkedList.cpp
+++ leetcode/linkedList/doublyLinkedList.cpp
@@ -15,10 +15,30 @@ public:
 
     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
     void addAtHead(int val) {
+        if (!length) {
+            auto newNode = new DoublyListNode(val);
+            head = newNode;
+            tail = newNode;
+        } else {
+            auto newNode = new DoublyListNode(val, nullptr, head);
+            head = newNode;
+        }
+
+        ++length;
     }
 
     /** Append a node of value val to the last element of the linked list. */
     void addAtTail(int val) {
+        if (!length) {
+            auto newNode = new DoublyListNode(val);
+            head = newNode;
+            tail = newNode;
+        } else {
+            auto newNode = new DoublyListNode(val, tail);
+            tail = newNode;
+        }
+
+        ++length;
     }
 
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
