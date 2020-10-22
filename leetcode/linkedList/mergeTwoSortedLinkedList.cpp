da48f30a988bd049506154a7a242093f15543d08
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-22 18:33:58 +0100

    Problem statement

---
+++ leetcode/linkedList/mergeTwoSortedLinkedList.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Oyekunle Oloyede on 22/10/2020.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class MergeSortedLists {
+private:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        ListNode() : val(0), next(nullptr) {}
+
+        ListNode(int x) : val(x), next(nullptr) {}
+
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+
+    }
+};
