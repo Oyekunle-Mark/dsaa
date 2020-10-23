aa5f75e4397b391650490f6887f8511f5ee69e80
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-23 10:18:51 +0100

    Removed old code

--- leetcode/linkedList/addTwoNumbers.cpp
+++ leetcode/linkedList/addTwoNumbers.cpp
@@ -28,38 +28,6 @@ private:
 
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        auto currentL1 = l1;
-        std::string l1ToString{};
 
-        while (currentL1) {
-            l1ToString = std::to_string(currentL1->val) + l1ToString;
-            currentL1 = currentL1->next;
-        }
-
-        auto currentL2 = l2;
-        std::string l2ToString{};
-
-        while (currentL2) {
-            l2ToString = std::to_string(currentL2->val) + l2ToString;
-            currentL2 = currentL2->next;
-        }
-
-        std::string sum = std::to_string(std::stoi(l1ToString) + std::stoi(l2ToString));
-
-        ListNode *newHead{};
-        ListNode *currentNode{};
-
-        for (int index{}; index < sum.length(); ++index) {
-            if (!index) {
-                newHead = new ListNode(std::stoi(std::string(1, sum.at(index))));
-                currentNode = newHead;
-            } else {
-                auto newNode = new ListNode(std::stoi(std::string(1, sum.at(index))));
-                currentNode->next = newNode;
-                currentNode = newNode;
-            }
-        }
-
-        return newHead;
     }
 };
