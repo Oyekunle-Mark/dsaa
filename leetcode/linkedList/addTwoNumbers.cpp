44e23dc9e4f8c261ed2833aafac38deddbc012d2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-23 10:53:30 +0100

    Second pass solution

--- leetcode/linkedList/addTwoNumbers.cpp
+++ leetcode/linkedList/addTwoNumbers.cpp
@@ -28,6 +28,34 @@ private:
 
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        int l1Value = l1->val;
+        int l2Value = l2->val;
+        l1 = l1->next;
+        l2 = l2->next;
+        auto *head = new ListNode((l1Value + l2Value) % 10);
+        auto currentNode = head;
+        int carry = (l1Value + l2Value) / 10;
 
+        while (l1 || l2 || carry) {
+            l1Value = 0;
+            l2Value = 0;
+
+            if (l1) {
+                l1Value = l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2) {
+                l2Value = l2->val;
+                l2 = l2->next;
+            }
+
+            auto newNode = new ListNode((l1Value + l2Value + carry) % 10);
+            currentNode->next = newNode;
+            currentNode = newNode;
+            carry = (l1Value + l2Value + carry) / 10;
+        }
+
+        return head;
     }
 };
