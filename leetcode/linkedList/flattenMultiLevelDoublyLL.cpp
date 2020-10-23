865b8d097d7af39f60ceb1880c83bc3af3be6e08
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-23 12:49:35 +0100

    Documented code

--- leetcode/linkedList/flattenMultiLevelDoublyLL.cpp
+++ leetcode/linkedList/flattenMultiLevelDoublyLL.cpp
@@ -13,7 +13,7 @@ public:
 };
 */
 
-class Solution {
+class FlattenAMultiLevelDoublyLinkedList {
 private:
     class Node {
     public:
@@ -25,72 +25,78 @@ private:
 
 public:
     Node *flatten(Node *head) {
-        if (head == nullptr) return head;
+        if (head == nullptr) return head; // empty head;
 
         auto currentNode = head;
 
-        while (currentNode) {
-            auto nextNode = currentNode->next;
+        while (currentNode) { // while there is a node to process
+            auto nextNode = currentNode->next; // preserve a pointer to the next node
 
-            if (currentNode->child) {
-                currentNode->next = currentNode->child;
-                currentNode->child->prev = currentNode;
-                auto newChildTail = mergeChildren(currentNode->child);
-                currentNode->child = nullptr;
+            if (currentNode->child) { // if current node has a child list
+                currentNode->next = currentNode->child; // point current node's next to the head of it's childnode
+                currentNode->child->prev = currentNode; // point current node's child head prev to current node
 
-                if (nextNode == nullptr) {
+                auto newChildTail = mergeChildren(
+                        currentNode->child); // call method to merge all child list and return pointer to the tail of the merged list
+                currentNode->child = nullptr; // set current node's child to null after merger
+
+                if (nextNode == nullptr) { // if at end of list, point merged child list tail's next to NULL and break
                     newChildTail->next = nextNode;
                     break;
                 }
 
-                newChildTail->next = nextNode;
-                nextNode->prev = newChildTail;
+                newChildTail->next = nextNode; // point merged child list tail's next to the next node
+                nextNode->prev = newChildTail; // point next node's prev to the merged child list tail
 
-                currentNode = nextNode;
-                continue;
+                currentNode = nextNode; // advance current node to next
+                continue; // skip to next iteration
             }
 
-            if (nextNode == nullptr) {
+            if (nextNode == nullptr) { // if at tail, point current node's next to NULL and break
                 currentNode->next = nextNode;
                 break;
             }
 
-            currentNode->next = nextNode;
-            nextNode->prev = currentNode;
-            currentNode = nextNode;
+            currentNode->next = nextNode; // point current node's next to the next node
+            nextNode->prev = currentNode; // point next node's prev to the current node
+            currentNode = nextNode; // advance pointer
         }
 
-        return head;
+        return head; // return head
     }
 
+    /**
+     * Recursively merges all child lists of a given ListNode and returns a pointer to the tail of the merged list.
+     */
     Node *mergeChildren(Node *currentHead) {
-        while (currentHead) {
-            auto nextNode = currentHead->next;
+        while (currentHead) { // while there is a node to process
+            auto nextNode = currentHead->next; // preserve a pointer to the next node
 
-            if (currentHead->child) {
-                currentHead->next = currentHead->child;
-                currentHead->child->prev = currentHead;
-                auto newChildTail = mergeChildren(currentHead->child);
-                currentHead->child = nullptr;
+            if (currentHead->child) { // if current node has a child
+                currentHead->next = currentHead->child; // point current node's next to the head of the child list
+                currentHead->child->prev = currentHead; // point child list's head prev to current node
 
-                if (nextNode == nullptr)
+                auto newChildTail = mergeChildren(currentHead->child); // recursively merge child lists
+                currentHead->child = nullptr; // set child to NULL after merger
+
+                if (nextNode == nullptr) // if at end of list, return the newChildTail as merged list tail
                     return newChildTail;
 
-                newChildTail->next = nextNode;
-                nextNode->prev = newChildTail;
+                newChildTail->next = nextNode; // point merged list tail's next to the next node
+                nextNode->prev = newChildTail; // point next node prev to merged list tail
 
-                currentHead = nextNode;
-                continue;
+                currentHead = nextNode; // advance current to next
+                continue; // skip to next iteration
             }
 
-            if (nextNode == nullptr)
+            if (nextNode == nullptr) // if at end break;
                 break;
 
-            currentHead->next = nextNode;
-            nextNode->prev = currentHead;
-            currentHead = nextNode;
+            currentHead->next = nextNode; // set current node's next to the next node
+            nextNode->prev = currentHead; // set next node's prev to the current node
+            currentHead = nextNode; // advance current to next
         }
 
-        return currentHead;
+        return currentHead; // return the last processed node as tail
     }
 };
