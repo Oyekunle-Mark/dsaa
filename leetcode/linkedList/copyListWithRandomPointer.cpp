90852a772543fb62d5d79bc026e5285cffd67b90
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-23 19:36:28 +0100

    first pass working solution

--- leetcode/linkedList/copyListWithRandomPointer.cpp
+++ leetcode/linkedList/copyListWithRandomPointer.cpp
@@ -1,6 +1,8 @@
 //
 // Created by Oyekunle Oloyede on 23/10/2020.
 //
+#include <map>
+
 /*
 // Definition for a Node.
 class Node {
@@ -34,40 +36,37 @@ private:
 
 public:
     Node *copyRandomList(Node *head) {
-        if (head == nullptr) return head;
+        if (head == nullptr) return head; // return for empty list
 
+        std::map<Node *, Node *> nodeMap{}; // stores a mapping of nodes to copies
         auto currentNode = head;
 
         while (currentNode) {
-            auto nextNode = currentNode->next;
+            auto newNode = new Node(currentNode->val); // create a copy with current node's val
+            nodeMap.insert({currentNode, newNode}); // insert the mapping
 
-            auto newNode = new Node(currentNode->val);
-            newNode->next = nextNode;
-            currentNode->next = newNode;
-            currentNode = nextNode;
+            currentNode = currentNode->next;
         }
 
-        auto prevNode = head;
-        auto nextNode = head->next;
-        head = nextNode;
+        auto newHead = nodeMap.at(head); // set the copy of the head node as new node
+        currentNode = head; // track current node
 
-        while (prevNode) {
-            if (prevNode->random)
-                nextNode->random = prevNode->random->next;
+        if (currentNode->random) // if current node has a random pointer
+            newHead->random = nodeMap.at(currentNode->random); // se
+        if (currentNode->next)
+            newHead->next = nodeMap.at(currentNode->next);
+        currentNode = currentNode->next;
+        auto currentNew = newHead->next;
 
-            auto prevPrev = prevNode;
-            prevNode = prevNode->next->next;
-            prevPrev->next = prevNode;
-
-            if (prevNode == nullptr)
-                break;
-
-            auto prevNext = nextNode;
-//            nextNode->next = nextNode->next->next;
-            nextNode = nextNode->next->next;
-            prevNext->next = nextNode;
+        while (currentNode) {
+            if (currentNode->random)
+                currentNew->random = nodeMap.at(currentNode->random);
+            if (currentNode->next)
+                currentNew->next = nodeMap.at(currentNode->next);
+            currentNode = currentNode->next;
+            currentNew = currentNew->next;
         }
 
-        return head;
+        return newHead;
     }
 };
