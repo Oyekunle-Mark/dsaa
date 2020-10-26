da662060650a5101df8c0e47d062763da2168d5e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-26 19:34:52 +0100

    Problem case for a binary tree post order traversal

---
+++ leetcode/tree/binaryTreePostOrderTraversal.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Oyekunle Oloyede on 26/10/2020.
+//
+#include <vector>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+        explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+public:
+    std::vector<int> postorderTraversal(TreeNode *root) {
+
+    }
+};
