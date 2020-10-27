3ec66bd5e9d0a25eef6075d786a8e2ac6ec73e7c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-27 18:34:07 +0100

    first pass solution

--- leetcode/tree/binaryTreeLevelOrderTraversal.cpp
+++ leetcode/tree/binaryTreeLevelOrderTraversal.cpp
@@ -30,6 +30,41 @@ private:
 
 public:
     std::vector<std::vector<int>> levelOrder(TreeNode *root) {
+        std::vector<std::vector<int>> levelOrder{};
+        auto treeHeight = findTreeHeight(root);
 
+        for (int level = 1; level <= treeHeight; ++level) {
+            std::vector<int> nodeVisitedAtThisLevel{};
+            visitTreeNodeAtLevel(root, level, nodeVisitedAtThisLevel);
+            levelOrder.push_back(nodeVisitedAtThisLevel);
+        }
+
+        return levelOrder;
+    }
+
+    int findTreeHeight(TreeNode *root, int currentLevel = 0) {
+        if (root == nullptr)
+            return currentLevel;
+
+        int heightOfLeftSubtree = findTreeHeight(root->left, currentLevel + 1);
+        int heightOfRightSubtree = findTreeHeight(root->right, currentLevel + 1);
+
+        return max(heightOfLeftSubtree, heightOfRightSubtree);
+    }
+
+    int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    void visitTreeNodeAtLevel(TreeNode *root, int level, std::vector<int> &nodesVisited) {
+        if (root == nullptr)
+            return;
+
+        if (level == 1)
+            nodesVisited.push_back(root->val);
+        else {
+            visitTreeNodeAtLevel(root->left, level - 1, nodesVisited);
+            visitTreeNodeAtLevel(root->right, level - 1, nodesVisited);
+        }
     }
 };
