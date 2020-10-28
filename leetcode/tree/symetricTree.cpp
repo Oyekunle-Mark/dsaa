b27a2dd48cc5087072a1370e503086e4b2424004
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-28 19:45:44 +0100

    First pass

--- leetcode/tree/symetricTree.cpp
+++ leetcode/tree/symetricTree.cpp
@@ -29,6 +29,16 @@ private:
 
 public:
     bool isSymmetric(TreeNode *root) {
+        return isMirror(root, root);
+    }
 
+    bool isMirror(TreeNode *root1, TreeNode *root2) {
+        if (root1 == nullptr && root2 == nullptr)
+            return true;
+
+        if (root1 && root2 && root1->val == root2->val)
+            return isMirror(root1->left, root2->right) && isMirror(root1->right, root2->left);
+
+        return false;
     }
 };
