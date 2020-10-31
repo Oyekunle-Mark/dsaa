6c3bf2a7012ed77f7e6193bca9817d46897ff843
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-31 22:44:20 +0100

    documented code

--- leetcode/tree/pathSum.cpp
+++ leetcode/tree/pathSum.cpp
@@ -29,25 +29,30 @@ private:
 
 public:
     bool hasPathSum(TreeNode *root, int sum) {
-        if (root == nullptr)
+        if (root == nullptr) // return false for empty tree
             return false;
 
-        return findPathSum(root, sum);
+        return findPathSum(root, sum, root->val); // call findPath starting at root and using root-val as current sum
     }
 
-    bool findPathSum(TreeNode *root, int target, int currentSum = 0) {
+    /**
+     * Recursively add up the sum of the node values along the tree.
+     * Compares currentSum to target and returns result of comparison.
+     */
+    bool findPathSum(TreeNode *root, int target, int currentSum) {
+        // base case. At leaf node, compare and return result of comparison
         if (root->left == nullptr && root->right == nullptr)
             return currentSum == target;
 
         auto leftSubTreePathSumEquals = false;
         auto rightSubTreePathSumEquals = false;
 
-        if (root->left)
+        if (root->left) // if there is a left node, pass left node pointer and add left node value to current sum
             leftSubTreePathSumEquals = findPathSum(root->left, target, currentSum + root->left->val);
 
-        if (root->right)
+        if (root->right) // if there is a right node, pass right node pointer and add right node value to current sum
             rightSubTreePathSumEquals = findPathSum(root->right, target, currentSum + root->right->val);
 
-        return leftSubTreePathSumEquals || rightSubTreePathSumEquals;
+        return leftSubTreePathSumEquals || rightSubTreePathSumEquals; // does sum of left or right sub-tree equal target
     }
 };
