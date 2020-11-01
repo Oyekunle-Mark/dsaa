69d7a41878f248c6f69d88c584d13f7021ea4ca0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-01 16:50:26 +0100

    Second pass

--- leetcode/recursion/reverseString.cpp
+++ leetcode/recursion/reverseString.cpp
@@ -6,15 +6,20 @@
 class Solution {
 public:
     void reverseString(std::vector<char> &s) {
-        if (s.size())
-            reverseRecursive(s, 0, s.size() - 1);
+        if (s.empty())
+            return;
+
+        reverseRecursive(s, 0, s.size() - 1);
     }
 
     void reverseRecursive(std::vector<char> &str, size_t front, size_t end) {
         if (front < end) {
+            // swap the char at front and end of str
             char temp = str.at(front);
             str.at(front) = str.at(end);
             str.at(end) = temp;
+
+            // call recursive function while advancing the front pointer forward and end pointer backwards
             reverseRecursive(str, front + 1, end - 1);
         }
     }
