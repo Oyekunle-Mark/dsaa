59a0ae1d8ec1ec119d645bbe5ed0c1452a0e7433
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-01 16:47:33 +0100

    First pass

--- leetcode/recursion/reverseString.cpp
+++ leetcode/recursion/reverseString.cpp
@@ -6,6 +6,16 @@
 class Solution {
 public:
     void reverseString(std::vector<char> &s) {
+        if (s.size())
+            reverseRecursive(s, 0, s.size() - 1);
+    }
 
+    void reverseRecursive(std::vector<char> &str, size_t front, size_t end) {
+        if (front < end) {
+            char temp = str.at(front);
+            str.at(front) = str.at(end);
+            str.at(end) = temp;
+            reverseRecursive(str, front + 1, end - 1);
+        }
     }
 };
