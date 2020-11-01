10e92f2bbfd71972e7c9572658ae9c0f88055a0a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-01 17:47:42 +0100

    REcursively reverse linked list problem

---
+++ leetcode/recursion/reverseLinkedList.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Oyekunle Oloyede on 01/11/2020.
+//
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class ReverseLinkedList {
+private:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        ListNode() : val(0), next(nullptr) {}
+
+        explicit ListNode(int x) : val(x), next(nullptr) {}
+
+        explicit ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+public:
+    ListNode *reverseList(ListNode *head) {
+        if (head == nullptr)
+            return head;
+
+        return recursivelyReverseList(head, head);
+    }
+
+    ListNode *recursivelyReverseList(ListNode *head, ListNode *oldHead) {
+
+    }
+};
