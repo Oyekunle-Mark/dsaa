5208efda383f26f6e15a0a6870a7bdfa6e49f02d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-01 18:51:27 +0100

    documented optimised second pass

--- leetcode/recursion/pascalsTriangleII.cpp
+++ leetcode/recursion/pascalsTriangleII.cpp
@@ -30,28 +30,35 @@ f(i,j)=1 where j=1 or j=i
 class Solution {
 public:
     std::vector<int> getRow(int rowIndex) {
-        std::vector<int> triangleRow{};
-        std::map<std::string, int> cache{};
+        std::vector<int> triangleRow{}; // to hold the Pascal triangle row
+        std::map<std::string, int> cache{}; // the cache for storing result of previous operations
 
-        int columnIndex = 0;
+        int columnIndex = 0; // start at first column
 
-        while (columnIndex <= rowIndex)
+        while (columnIndex <= rowIndex) // loop while column is less than or equals row
+            // add the result of recursive computation of current row and column index and post increment column index
             triangleRow.push_back(getPascalNumber(rowIndex, columnIndex++, cache));
 
         return triangleRow;
     }
 
+    /**
+     * Recursively compute the value of row-th column-th Pascal triangle number.
+     * Cache result and use previous result to optimize performance
+     */
     int getPascalNumber(int row, int column, std::map<std::string, int> &cache) {
-        if (column == 0 || row == column)
+        if (column == 0 || row == column) // base case
             return 1;
 
+        // the cache key
         auto key = std::to_string(row) + '-' + std::to_string(column);
 
         auto itr = cache.find(key);
 
-        if (itr != cache.end())
+        if (itr != cache.end()) // if key is found in cache, return early with the value
             return itr->second;
 
+        // otherwise recursively call find value and add to cache
         auto answer = getPascalNumber(row - 1, column - 1, cache) + getPascalNumber(row - 1, column, cache);
         cache.insert({key, answer});
 
