cf8b3013a44361fb90880ded06024695c9f274ac
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-02 18:17:43 +0100

    Second pass using memoization

--- leetcode/recursion/fibonacciNumber.cpp
+++ leetcode/recursion/fibonacciNumber.cpp
@@ -1,13 +1,30 @@
 //
 // Created by Oyekunle Oloyede on 02/11/2020.
 //
+#include <unordered_map>
 
 class Solution {
 public:
     int fib(int N) {
-        if (N < 2)
-            return N;
+        std::unordered_map<int, int> cache{};
+
+        return findFib(N, cache);
+    }
+
+    int findFib(int n, std::unordered_map<int, int> &cache) {
+        auto itr = cache.find(n);
+
+        if (itr != cache.end())
+            return itr->second;
+
+        int result{};
+
+        if (n < 2)
+            return n;
         else
-            return fib(N - 1) + fib(N - 2);
+            result = findFib(n - 1, cache) + findFib(n - 2, cache);
+
+        cache.insert({n, result});
+        return result;
     }
 };
