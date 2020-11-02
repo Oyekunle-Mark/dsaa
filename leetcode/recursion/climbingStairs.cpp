faab9325beb6a837f1a33ffd6fe9028cf21a0c68
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-02 18:31:20 +0100

    Optimized second pass

--- leetcode/recursion/climbingStairs.cpp
+++ leetcode/recursion/climbingStairs.cpp
@@ -1,13 +1,30 @@
 //
 // Created by Oyekunle Oloyede on 02/11/2020.
 //
+#include <unordered_map>
 
 class Solution {
 public:
     int climbStairs(int n) {
+        std::unordered_map<int, int> cache{};
+
+        return findStairsCombo(n, cache);
+    }
+
+    int findStairsCombo(int n, std::unordered_map<int, int> &cache) {
+        auto itr = cache.find(n);
+
+        if (itr != cache.end())
+            return itr->second;
+
+        int result = 0;
+
         if (n < 3)
             return n;
         else
-            return climbStairs(n - 1) + climbStairs(n - 2);
+            result = findStairsCombo(n - 1, cache) + findStairsCombo(n - 2, cache);
+
+        cache.insert({n, result});
+        return result;
     }
 };
