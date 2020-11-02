f53de70b33a6b1d4d8232f140eb848b374e55333
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-02 18:19:10 +0100

    Documented code

--- leetcode/recursion/fibonacciNumber.cpp
+++ leetcode/recursion/fibonacciNumber.cpp
@@ -6,12 +6,16 @@
 class Solution {
 public:
     int fib(int N) {
-        std::unordered_map<int, int> cache{};
+        std::unordered_map<int, int> cache{}; // the cache for memoization
 
         return findFib(N, cache);
     }
 
+    /**
+     * Recursively finds the nth fibonacci number. Uses memoization
+     */
     int findFib(int n, std::unordered_map<int, int> &cache) {
+        // check cache of input and return if previously computed
         auto itr = cache.find(n);
 
         if (itr != cache.end())
@@ -19,12 +23,12 @@ public:
 
         int result{};
 
-        if (n < 2)
+        if (n < 2) // base case
             return n;
         else
             result = findFib(n - 1, cache) + findFib(n - 2, cache);
 
-        cache.insert({n, result});
+        cache.insert({n, result}); // cache result
         return result;
     }
 };
