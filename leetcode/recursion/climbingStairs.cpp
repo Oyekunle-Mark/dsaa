aa3521786fd412ad6ae2001803b1ee25a91207cd
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-02 18:36:31 +0100

    Documented second pass

--- leetcode/recursion/climbingStairs.cpp
+++ leetcode/recursion/climbingStairs.cpp
@@ -11,7 +11,12 @@ public:
         return findStairsCombo(n, cache);
     }
 
+    /**
+     * Recursively find the number of ways of climbing n stairs.
+     * Uses memoization with an map as the cache.
+     */
     int findStairsCombo(int n, std::unordered_map<int, int> &cache) {
+        // check if operation has been performed previously. If it has return result from cache.
         auto itr = cache.find(n);
 
         if (itr != cache.end())
@@ -19,12 +24,13 @@ public:
 
         int result = 0;
 
+        // the base cases are there are 2 ways of climbing two stairs and 1 way of climbing one stair
         if (n < 3)
             return n;
-        else
+        else // recursively reduce n until it gets to the base case and add up result of sub-problems.
             result = findStairsCombo(n - 1, cache) + findStairsCombo(n - 2, cache);
 
-        cache.insert({n, result});
+        cache.insert({n, result}); // cache result
         return result;
     }
 };
