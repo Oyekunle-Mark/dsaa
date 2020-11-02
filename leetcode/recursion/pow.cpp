28e1263948a8ac7df5c8ecb3e2cbbfee7c95b288
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-02 19:44:43 +0100

    time limit exceeded

--- leetcode/recursion/pow.cpp
+++ leetcode/recursion/pow.cpp
@@ -5,6 +5,12 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
 
+        bool isPositivePower = n > 0;
+
+        auto newN = isPositivePower ? n - 1 : n + 1;
+        return isPositivePower ? x * myPow(x, newN) : 1 / x * myPow(x, newN);
     }
 };
