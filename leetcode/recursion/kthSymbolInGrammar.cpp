d57724211d44d325de030cbbca828276b25bbd09
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-05 18:58:19 +0100

    Recursive func template

--- leetcode/recursion/kthSymbolInGrammar.cpp
+++ leetcode/recursion/kthSymbolInGrammar.cpp
@@ -1,10 +1,15 @@
 //
 // Created by Oyekunle Oloyede on 05/11/2020.
 //
+#include <string>
 
 class Solution {
 public:
     int kthGrammar(int N, int K) {
+        return buildGrammarAndFindKthSymbol(N - 1, K, std::to_string(1));
+    }
+
+    int buildGrammarAndFindKthSymbol(int n, int k, std::string grammar) {
 
     }
 };
