5c3eef9369659158fe9646ea863cff7eff9875de
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-05 18:20:15 +0100

    Start condition

--- leetcode/recursion/mergeTwoSortedLists.cpp
+++ leetcode/recursion/mergeTwoSortedLists.cpp
@@ -27,6 +27,25 @@ private:
 
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if (l1 == nullptr) return l1;
+        if (l2 == nullptr) return l2;
+
+        ListNode *head{};
+
+        if (l1->val < l2->val) {
+            head = l1;
+            l1 = l1->next;
+        } else {
+            head = l2;
+            l2 = l2->next;
+        }
+
+        mergeLists(l1, l2, head);
+
+        return head;
+    }
+
+    void mergeLists(ListNode *l1, ListNode *l2, ListNode *mergedNode) {
 
     }
 };
