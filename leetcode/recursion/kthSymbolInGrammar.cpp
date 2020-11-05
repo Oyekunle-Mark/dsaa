7b935b36be5bdd41849d023f55078793d21c26dc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-05 19:10:27 +0100

    First pass

--- leetcode/recursion/kthSymbolInGrammar.cpp
+++ leetcode/recursion/kthSymbolInGrammar.cpp
@@ -6,10 +6,22 @@
 class Solution {
 public:
     int kthGrammar(int N, int K) {
-        return buildGrammarAndFindKthSymbol(N - 1, K, std::to_string(1));
+        return buildGrammarAndFindKthSymbol(N - 1, K, std::to_string(0));
     }
 
-    int buildGrammarAndFindKthSymbol(int n, int k, std::string grammar) {
+    int buildGrammarAndFindKthSymbol(int n, int k, const std::string &grammar) {
+        if (n == 0)
+            return grammar.at(k - 1) - 48;
 
+        std::string newGrammar{};
+
+        for (char c : grammar) {
+            if (c == '0')
+                newGrammar += "01";
+            else
+                newGrammar += "10";
+        }
+
+        return buildGrammarAndFindKthSymbol(n - 1, k, newGrammar);
     }
 };
