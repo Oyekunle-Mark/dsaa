b2611185b71f046f8d128af0528ce9bae3fcc531
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-09 11:05:16 +0100

    Cleared main

--- main.cpp
+++ main.cpp
@@ -2,19 +2,6 @@
 #include "leetcode/linkedList/doublyLinkedList.cpp"
 
 int main() {
-//    MyLinkedList myLinkedList{};
-//    myLinkedList.addAtHead(1);
-//    myLinkedList.print();
-//    myLinkedList.addAtTail(3);
-//    myLinkedList.print();
-//    myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
-//    myLinkedList.print();
-//    std::cout << myLinkedList.get(1) << '\n';              // return 2
-//    myLinkedList.deleteAtIndex(1);          // now the linked list is 1->3
-//    myLinkedList.print();
-//    std::cout << myLinkedList.get(1) << '\n';       // return 3
-//    myLinkedList.print();
-
     MyLinkedList myLinkedList{};
     myLinkedList.addAtHead(7);
     myLinkedList.addAtHead(2);
