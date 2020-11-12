c9aad58c7341dc169e29b8c93f73082f5b6379f9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-12 08:51:40 +0100

    n-ary tree level order traversal

---
+++ leetcode/n-AryTrees/nAryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Oyekunle Oloyede on 12/11/2020.
+//
+#include <vector>
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    std::vector<Node *> children;
+
+    Node() = default;
+
+    explicit Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, std::vector<Node *> _children) {
+        val = _val;
+        children = std::move(_children);
+    }
+};
+
+
+class Solution {
+public:
+    std::vector<std::vector<int>> levelOrder(Node *root) {
+
+    }
+};
