6da2bd9ef65ea8c95cca3a81b2d7a0c68b62f38d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-12 08:45:03 +0100

    n-ary post order traversal problem

---
+++ leetcode/n-AryTrees/nAryTreePostorderTraversal.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Oyekunle Oloyede on 12/11/2020.
+//
+
+//
+// Created by Oyekunle Oloyede on 12/11/2020.
+//
+#include <vector>
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    std::vector<Node *> children;
+
+    Node() = default;
+
+    explicit Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, std::vector<Node *> _children) {
+        val = _val;
+        children = std::move(_children);
+    }
+};
+
+
+class Solution {
+public:
+    std::vector<int> postorder(Node *root) {
+    }
+};
