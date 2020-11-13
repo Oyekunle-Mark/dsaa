c3d36342840f62a19bada1a323d220bf033ff18d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-13 11:24:49 +0100

    Nice first pass

--- leetcode/bst/searchInABinarySearchTree.cpp
+++ leetcode/bst/searchInABinarySearchTree.cpp
@@ -29,6 +29,15 @@ private:
 
 public:
     TreeNode *searchBST(TreeNode *root, int val) {
+        if (root == nullptr)
+            return nullptr;
 
+        if (root->val == val)
+            return root;
+
+        if (val < root->val)
+            return searchBST(root->left, val);
+
+        return searchBST(root->right, val);
     }
 };
