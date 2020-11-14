b34df7993cfc259838694a469d9e850e7a313e73
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-14 20:08:45 +0100

    The insert into bst problem

---
+++ leetcode/bst/insertIntoBST.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Oyekunle Oloyede on 14/11/2020.
+//
+
+class Solution {
+private:
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+        explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+public:
+    TreeNode *insertIntoBST(TreeNode *root, int val) {
+
+    }
+};
