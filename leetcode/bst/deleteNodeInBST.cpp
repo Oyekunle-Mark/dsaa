dcaa6814e9c1d97f93f3bac35ee29b5d0c87faf6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-14 21:26:25 +0100

    The delete node problem

---
+++ leetcode/bst/deleteNodeInBST.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Oyekunle Oloyede on 14/11/2020.
+//
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+        explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+public:
+    TreeNode *deleteNode(TreeNode *root, int key) {
+
+    }
+};
