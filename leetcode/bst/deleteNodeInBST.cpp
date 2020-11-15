f142908a4000675ec07f4735b1ae86799a48c4d2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-15 09:39:34 +0100

    Removed useless code

--- leetcode/bst/deleteNodeInBST.cpp
+++ leetcode/bst/deleteNodeInBST.cpp
@@ -47,32 +47,10 @@ public:
                 return temp;
             }
 
-            TreeNode *temp{};
-            findInOrderSuccessor(root, temp, root->val);
+            auto temp = findMin(root->right);
             root->val = temp->val;
-            root->right = deleteNode(root, temp->val);
+            root->right = deleteNode(root->right, temp->val);
         }
-
-//        auto targetNode = findTargetNode(root, key);
-//
-//        if (targetNode == nullptr)
-//            return root;
-//
-//        if (targetNode->left == nullptr && targetNode->right == nullptr) {
-////            delete targetNode;
-//            targetNode = nullptr;
-//        } else if (targetNode->left == nullptr || targetNode->right == nullptr) {
-//            auto childNode = targetNode->right == nullptr ? targetNode->left : targetNode->right;
-//            delete targetNode;
-//            targetNode = childNode;
-//        } else {
-//            TreeNode *successor{};
-//            findInOrderSuccessor(root, successor, targetNode->val);
-//
-//            targetNode->val = successor->val;
-////            delete successor;
-//            successor = nullptr;
-//        }
 
         return root;
     }
@@ -82,33 +60,5 @@ public:
             root = root->left;
 
         return root;
-    }
-
-    void findInOrderSuccessor(TreeNode *root, TreeNode *&successor, int val) {
-        if (root == nullptr) {
-            successor = nullptr;
-            return;
-        }
-
-        if (val < root->val) {
-            successor = root;
-            findInOrderSuccessor(root->left, successor, val);
-        } else if (val == root->val) {
-            if (root->right)
-                successor = findMin(root->right);
-        } else
-            findInOrderSuccessor(root->right, successor, val);
-    }
-
-    TreeNode *findTargetNode(TreeNode *root, int val) {
-        if (root == nullptr)
-            return nullptr;
-
-        if (root->val == val)
-            return root;
-        else if (val < root->val)
-            return findTargetNode(root->left, val);
-
-        return findTargetNode(root->right, val);
     }
 };
