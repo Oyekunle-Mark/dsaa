72a995cd74e14371d592051282429f9bffe3449f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-16 19:26:05 +0100

    The kth largest element in a stream problem

---
+++ leetcode/bst/kthLargestElementInAStream.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Oyekunle Oloyede on 16/11/2020.
+//
+#include <vector>
+
+class KthLargest {
+public:
+    KthLargest(int k, std::vector<int> &nums) {
+
+    }
+
+    int add(int val) {
+
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
