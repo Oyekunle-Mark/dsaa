62af7968c18ef6531cb5cf164401d54ad0a3dcc9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-17 08:59:56 +0100

    The implement a trie class problem

---
+++ leetcode/trie/implementTrie.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Oyekunle Oloyede on 17/11/2020.
+//
+#include <string>
+
+class Trie {
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(std::string word) {
+
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(std::string word) {
+
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(std::string prefix) {
+
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
