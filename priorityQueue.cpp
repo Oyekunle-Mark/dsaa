415c6466ecfef1964f2916d1ec9a89e174ede3cb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-24 19:45:47 +0100

    Bubbles a queue node to the correct position

--- priorityQueue.cpp
+++ priorityQueue.cpp
@@ -10,3 +10,18 @@ void PriorityQueue<DataType>::swap(int index1, int index2) {
     items.at(index1) = items.at(index2);
     items.at(index2) = temp;
 }
+
+template<typename DataType>
+void PriorityQueue<DataType>::bubbleUp() {
+    int index = static_cast<int>(items.size() - 1);
+
+    while (index > 0) {
+        int parentIndex = (index - 1) / 2;
+
+        if (items.at(index) < items.at(parentIndex)) {
+            swap(parentIndex, index);
+            index = parentIndex;
+        } else
+            break;
+    }
+}
