81ec8c390456da8a48b5b9975d738e73fbfb141d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-02 09:31:06 +0100

    First pass at find duplicate

--- leetcode/hashMap/containsDuplicates.cpp
+++ leetcode/hashMap/containsDuplicates.cpp
@@ -2,10 +2,20 @@
 // Created by Oyekunle Oloyede on 02/12/2020.
 //
 #include <vector>
+#include <unordered_set>
 
 class Solution {
 public:
     bool containsDuplicate(std::vector<int> &nums) {
+        std::unordered_set<int> store{};
 
+        for (auto item : nums) {
+            if (store.count(item))
+                return true;
+
+            store.insert(item);
+        }
+
+        return false;
     }
 };
