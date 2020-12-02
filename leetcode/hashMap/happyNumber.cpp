5cd2a4109aef7bbd1aa43f37b0aff1e23dac7561
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-02 10:24:38 +0100

    isHappy method loops until sum is 1 or detects a cycle

--- leetcode/hashMap/happyNumber.cpp
+++ leetcode/hashMap/happyNumber.cpp
@@ -1,11 +1,25 @@
 //
 // Created by Oyekunle Oloyede on 02/12/2020.
 //
+#include <set>
 
 class Solution {
 public:
     bool isHappy(int n) {
+        std::set<int> sums{};
 
+        while (true) {
+            n = findDigitSquareSum(n);
+
+            if (n == 1)
+                return true;
+            else if (sums.find(n) == sums.end())
+                sums.insert(n);
+            else
+                break;
+        }
+
+        return false;
     }
 
     int findDigitSquareSum(int num) {
