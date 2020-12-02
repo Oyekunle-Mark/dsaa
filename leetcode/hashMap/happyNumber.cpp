882975df9ad5c3f615d37e9c787de78ff8fbf40b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-02 10:21:43 +0100

    helper method to find the sum of the square of th digit in a number

--- leetcode/hashMap/happyNumber.cpp
+++ leetcode/hashMap/happyNumber.cpp
@@ -7,4 +7,16 @@ public:
     bool isHappy(int n) {
 
     }
+
+    int findDigitSquareSum(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            int last = num % 10;
+            sum += (last * last);
+            num /= 10;
+        }
+
+        return sum;
+    }
 };
