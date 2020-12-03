baa844817a8e47acd4c0bbdb6c8fca6d6b432f51
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-03 12:11:34 +0100

    Appended the first pass

--- leetcode/hashMap/minimumIndexSumOfTwoLists.cpp
+++ leetcode/hashMap/minimumIndexSumOfTwoLists.cpp
@@ -8,27 +8,30 @@
 class Solution {
 public:
     std::vector<std::string> findRestaurant(std::vector<std::string> &list1, std::vector<std::string> &list2) {
-        std::unordered_map<std::string, int> firstListIndexMap{};
+        std::unordered_map<std::string, int> firstListIndexMap{}; // stores a map of restaurants in list1 to index
 
-        for (int index = 0; index < list1.size(); ++index)
+        for (int index = 0; index < list1.size(); ++index) // loop and set the index of each restaurant in list1
             firstListIndexMap.insert({list1.at(index), index});
 
-        std::unordered_map<std::string, int> commonInterestIndicesSumMap{};
-        int minimumListIndexSum = 1000 * 2;
+        std::unordered_map<std::string, int> commonInterestIndicesSumMap{}; // will hold the sum of indices of restaurant present in list1 and list2
+        int minimumListIndexSum = 1000 *
+                                  2; // maximum list size is 1000. So highest possible index is an item at the tail end of both max list
 
-        for (int index = 0; index < list2.size(); ++index) {
-            if (firstListIndexMap.count(list2.at(index))) {
-                commonInterestIndicesSumMap.insert({list2.at(index), firstListIndexMap.at(list2.at(index)) + index});
+        for (int index = 0; index < list2.size(); ++index) { // loop through list 2
+            if (firstListIndexMap.count(list2.at(index))) { // if current restaurant is found in list 1
+                commonInterestIndicesSumMap.insert({list2.at(index), firstListIndexMap.at(list2.at(index)) +
+                                                                     index}); // retrieves its index from list 1 and add its index in list two. Save restaurant as a map to the sum of these indices.
 
-                if (commonInterestIndicesSumMap.at(list2.at(index)) < minimumListIndexSum)
-                    minimumListIndexSum = commonInterestIndicesSumMap.at(list2.at(index));
+                if (commonInterestIndicesSumMap.at(list2.at(index)) <
+                    minimumListIndexSum) // if sum of indices of current common restaurant is less than the minimum
+                    minimumListIndexSum = commonInterestIndicesSumMap.at(list2.at(index)); // set as new minimum
             }
         }
 
         std::vector<std::string> interests{};
 
-        for (const std::pair<std::string, int> pair : commonInterestIndicesSumMap) {
-            if (pair.second == minimumListIndexSum)
+        for (const std::pair<std::string, int> pair : commonInterestIndicesSumMap) { // for each pair
+            if (pair.second == minimumListIndexSum) // if it's a minimum, append restaurant to interest
                 interests.push_back(pair.first);
         }
 
