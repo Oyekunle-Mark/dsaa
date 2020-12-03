d151c38bfaba55223a182017e3520d1d3a681a91
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-03 10:08:24 +0100

    The two sum probelm

---
+++ leetcode/hashMap/twoSum.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Oyekunle Oloyede on 03/12/2020.
+//
+
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+
+    }
+};
