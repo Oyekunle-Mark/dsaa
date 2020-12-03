46981d40595310fcb9e9232c28627c3f3b1e96f9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-03 11:26:23 +0100

    Commented code

--- leetcode/hashMap/isormorphicStrings.cpp
+++ leetcode/hashMap/isormorphicStrings.cpp
@@ -7,20 +7,22 @@
 class Solution {
 public:
     bool isIsomorphic(std::string s, std::string t) {
-        std::unordered_map<char, char> sMap{};
-        std::unordered_map<char, char> tMap{};
+        std::unordered_map<char, char> sMap{}; // tracks mapping of s characters
+        std::unordered_map<char, char> tMap{}; // tracks mapping of t characters
 
-        for (std::size_t index = 0; index < s.size(); ++index) {
-            if (sMap.count(s.at(index))) {
-                if (sMap.at(s.at(index)) != t.at(index))
+        for (std::size_t index = 0;
+             index < s.size(); ++index) { // since s and t lengths are the same, loop with s length
+            if (sMap.count(s.at(index))) { // if current s character has been mapped already
+                if (sMap.at(s.at(index)) != t.at(index)) // ensure it maps to the same current character in t
                     return false;
             } else {
-                if (tMap.count(t.at(index)))
-                    if (tMap.at(t.at(index)) != s.at(index))
+                if (tMap.count(t.at(index))) // check if there is a previous mapping for t current character
+                    if (tMap.at(t.at(index)) !=
+                        s.at(index)) // ensure it maps to the current s character before performing new map
                         return false;
 
-                sMap.insert({s.at(index), t.at(index)});
-                tMap.insert({t.at(index), s.at(index)});
+                sMap.insert({s.at(index), t.at(index)}); // create new s character mapping
+                tMap.insert({t.at(index), s.at(index)}); // create new t character mapping
             }
         }
 
