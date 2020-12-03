68ea309b913888b2f6be1096d92f24b4255f6e75
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-03 10:22:11 +0100

    First pass at two sum probelm

--- leetcode/hashMap/twoSum.cpp
+++ leetcode/hashMap/twoSum.cpp
@@ -3,10 +3,23 @@
 //
 
 #include <vector>
+#include <unordered_map>
 
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::vector<int> twoSum(std::vector<int> &nums, int target) {
+        std::unordered_map<int, int> numberIndexMap{};
+        std::vector<int> twoSumIndices{};
 
+        for (int index = 0; index < nums.size(); ++index) {
+            if (numberIndexMap.count(target - nums.at(index))) {
+                twoSumIndices.push_back(numberIndexMap.at(target - nums.at(index)));
+                twoSumIndices.push_back(index);
+                break;
+            } else
+                numberIndexMap.insert({nums.at(index), index});
+        }
+
+        return twoSumIndices;
     }
 };
