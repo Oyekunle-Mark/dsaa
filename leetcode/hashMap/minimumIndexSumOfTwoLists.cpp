0c295c5ae6c5ebe5097334a8eb5de80af34e473e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-03 11:46:06 +0100

    the Minimum Index Sum of Two Lists problem

---
+++ leetcode/hashMap/minimumIndexSumOfTwoLists.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Oyekunle Oloyede on 03/12/2020.
+//
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    std::vector<std::string> findRestaurant(std::vector<std::string> &list1, std::vector<std::string> &list2) {
+
+    }
+};
