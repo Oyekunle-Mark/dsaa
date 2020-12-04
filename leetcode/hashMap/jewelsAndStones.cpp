4755d882a90e778bb61f87aa43d5200862431a4e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-04 09:36:50 +0100

    First pass solution for the num jewels in stone problem

--- leetcode/hashMap/jewelsAndStones.cpp
+++ leetcode/hashMap/jewelsAndStones.cpp
@@ -2,10 +2,23 @@
 // Created by Oyekunle Oloyede on 04/12/2020.
 //
 #include <string>
+#include <unordered_set>
 
 class Solution {
 public:
     int numJewelsInStones(std::string J, std::string S) {
+        std::unordered_set<char> jewels{};
 
+        for (auto c : J)
+            jewels.insert(c);
+
+        int count = 0;
+
+        for (auto c : S) {
+            if (jewels.count(c))
+                ++count;
+        }
+
+        return count;
     }
 };
