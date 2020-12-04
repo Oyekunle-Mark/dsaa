9c84cbc18f3926ff18c7e08919bafb046c5ed32d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-04 09:04:52 +0100

    Finds the group of a string by adding the integer values of its characters

--- leetcode/hashMap/groupAnagrams.cpp
+++ leetcode/hashMap/groupAnagrams.cpp
@@ -3,10 +3,20 @@
 //
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 class Solution {
 public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string> &strs) {
+        std::unordered_map<int, std::vector<std::string>> groups{};
+    }
 
+    int findGroup(const std::string &str) {
+        int sum = 0;
+
+        for (auto c : str)
+            sum += c;
+
+        return sum;
     }
 };
