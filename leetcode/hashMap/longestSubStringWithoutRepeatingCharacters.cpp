67ac4c81e9951ba47a9eeed10137ef929989b4c2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-04 09:47:10 +0100

    First pass

--- leetcode/hashMap/longestSubStringWithoutRepeatingCharacters.cpp
+++ leetcode/hashMap/longestSubStringWithoutRepeatingCharacters.cpp
@@ -2,10 +2,26 @@
 // Created by Oyekunle Oloyede on 04/12/2020.
 //
 #include <string>
+#include <unordered_set>
 
 class Solution {
 public:
     int lengthOfLongestSubstring(std::string s) {
+        std::unordered_set<char> previousCharacters{};
+        int max = 0;
+        int current = 0;
 
+        for (auto c : s) {
+            if (previousCharacters.count(c)) {
+                previousCharacters.clear();
+                current = 0;
+            }
+
+            previousCharacters.insert(c);
+            if (++current > max)
+                max = current;
+        }
+
+        return max;
     }
 };
