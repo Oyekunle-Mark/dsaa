344ac129786ef0cc23ae94872f808a3c883bbf8a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-04 09:11:38 +0100

    First pass

--- leetcode/hashMap/groupAnagrams.cpp
+++ leetcode/hashMap/groupAnagrams.cpp
@@ -9,6 +9,22 @@ class Solution {
 public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string> &strs) {
         std::unordered_map<int, std::vector<std::string>> groups{};
+
+        for (const auto &str : strs) {
+            auto groupNumber = findGroup(str);
+
+            if (groups.count(groupNumber))
+                groups.at(groupNumber).push_back(str);
+            else
+                groups.insert({groupNumber, {str}});
+        }
+
+        std::vector<std::vector<std::string>> groupedAnagrams{};
+
+        for (auto pair : groups)
+            groupedAnagrams.push_back(pair.second);
+
+        return groupedAnagrams;
     }
 
     int findGroup(const std::string &str) {
