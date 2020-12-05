9c5261fc1db9c2a2ba0369b03a3dc0169efd52c9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-05 08:17:57 +0100

    Find dynamic array sumk

--- leetcode/arraysAndString/findPivotIndex.cpp
+++ leetcode/arraysAndString/findPivotIndex.cpp
@@ -1,10 +1,16 @@
 //
 // Created by Oyekunle Oloyede on 05/12/2020.
 //
+#include <algorithm>
 #include <vector>
 
 class Solution {
 public:
     int pivotIndex(std::vector<int> &nums) {
+        int sum = 0;
+        std::for_each(nums.begin(), nums.end(),
+                      [&sum](int num) {
+                          sum += num;
+                      })
     }
 };
