fe98afbd4d27cb32f875cc536dfeb921317ab717
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-05 09:02:44 +0100

    first pass

--- leetcode/arraysAndString/largestNumberAtLeastTwiceOthers.cpp
+++ leetcode/arraysAndString/largestNumberAtLeastTwiceOthers.cpp
@@ -2,10 +2,24 @@
 // Created by Oyekunle Oloyede on 05/12/2020.
 //
 #include <vector>
+#include <algorithm>
 
 class Solution {
 public:
-    int dominantIndex(vector<int>& nums) {
+    int dominantIndex(vector<int> &nums) {
+        int largestIndex = 0;
 
+        for (int index = 0; index < nums.size(); ++index) {
+            if (nums.at(index) > nums.at(largestIndex))
+                largestIndex = index;
+        }
+
+        for (int index = 0; index < nums.size(); ++index) {
+            if (index != largestIndex)
+                if (nums.at(index) * 2 > nums.at(largestIndex))
+                    return -1;
+        }
+
+        return largestIndex;
     }
 };
