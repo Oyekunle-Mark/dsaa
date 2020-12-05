4be972655c6653ce9344756d35325304d6dc6052
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-12-05 09:31:36 +0100

    built up the incremented integer with a doublylinked list

--- leetcode/arraysAndString/plusOne.cpp
+++ leetcode/arraysAndString/plusOne.cpp
@@ -2,10 +2,40 @@
 // Created by Oyekunle Oloyede on 05/12/2020.
 //
 #include <vector>
+#include <list>
+#include <algorithm>
 
 class Solution {
 public:
-    std::vector<int> plusOne(std::vector<int>& digits) {
+    std::vector<int> plusOne(std::vector<int> &digits) {
+        bool carry = false;
+        std::list<int> incrementedDigits{};
 
+        for (int index = digits.size() - 1; index >= 0; --index) {
+            if (index == digits.size() - 1) {
+                if (digits.at(index) == 9) {
+                    incrementedDigits.insert(incrementedDigits.begin(), 0);
+                    carry = true;
+                } else {
+                    incrementedDigits.insert(incrementedDigits.begin(), digits.at(index) + 1);
+                }
+            } else {
+                if (carry) {
+                    if (digits.at(index) == 9) {
+                        incrementedDigits.insert(incrementedDigits.begin(), 0);
+                        carry = true;
+                    } else {
+                        incrementedDigits.insert(incrementedDigits.begin(), digits.at(index) + 1);
+                        carry = false;
+                    }
+                } else {
+                    incrementedDigits.insert(incrementedDigits.begin(), digits.at(index) + 1);
+                }
+
+            }
+        }
+
+        if (carry)
+            incrementedDigits.insert(incrementedDigits.begin(), 1);
     }
 };
